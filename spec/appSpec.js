6537c18f439ce358838aa7678068fb3a5a5fca26
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2025-01-16 22:12:38 +0100

    first commit

---
+++ spec/appSpec.js
@@ -0,0 +1,10 @@
+import { Main } from '../src/app.js';
+
+describe("A suite of test for the demo function of the main class", function() {
+    
+    it("demo", function() {
+        expect(1).toBe(1);
+    });
+    
+});
+
